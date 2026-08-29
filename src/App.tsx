a26e954574f57c59eb387ad729ccba9746b7577b
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-08-29 22:29:19 +0300

    feat: add routes for CreateArmyPage and ArmyPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {Route, Routes} from "react-router";
 import RouterLayout from "./components/RouterLayout.tsx";
 import ArmyListPage from "./pages/ArmyListPage.tsx";
+import CreateArmyPage from "./pages/CreateArmyPage.tsx";
+import ArmyPage from "./pages/ArmyPage.tsx";
 
 function App() {
 
@@ -8,7 +10,22 @@ function App() {
         <Routes>
 
             <Route element={<RouterLayout/>}>
-                <Route index element={<ArmyListPage/>}/>
+
+                <Route
+                    index
+                    element={<ArmyListPage/>}
+                />
+
+                <Route
+                    path="armies/create"
+                    element={<CreateArmyPage/>}
+                />
+
+                <Route
+                    path="armies/:armyId"
+                    element={<ArmyPage/>}
+                />
+
             </Route>
 
         </Routes>
