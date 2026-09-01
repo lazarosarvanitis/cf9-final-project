08cc51c7f89f2840eb79f4e25a52d72374e600f2
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-09-01 18:55:07 +0300

    feat: add CORS middleware to enable cross-origin requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 
 from app.database import Base, engine
@@ -11,6 +12,7 @@ from app.models.user import User
 from app.routers import (
     army_router,
     army_unit_router,
+    auth_router,
     detachment_router,
     faction_router,
     unit_router
@@ -25,11 +27,24 @@ app = FastAPI(
 )
 
 
+app.add_middleware(
+    CORSMiddleware,               #Connects frontend to backend
+    allow_origins=[               #please work
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 app.include_router(faction_router.router)
 app.include_router(detachment_router.router)
 app.include_router(unit_router.router)
 app.include_router(army_router.router)
 app.include_router(army_unit_router.router)
+app.include_router(auth_router.router)
 
 
 @app.get("/")
@@ -41,10 +56,10 @@ def home():
 
 @app.get("/database-test")
 def database_test():
-
     with engine.connect() as connection:
         connection.execute(text("SELECT 1"))
 
     return {
         "message": "Database connection successful"
     }
+
