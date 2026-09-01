3ab48a1b43ad7e3da43b5695cbd61c7d3bb8f3f7
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-09-01 18:54:55 +0300

    feat: add LoginPage route and wrap protected routes with ProtectedRoute component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
 import {Route, Routes} from "react-router";
+
 import RouterLayout from "./components/RouterLayout.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+
 import ArmyListPage from "./pages/ArmyListPage.tsx";
 import CreateArmyPage from "./pages/CreateArmyPage.tsx";
 import ArmyPage from "./pages/ArmyPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+
 
 function App() {
 
     return (
         <Routes>
 
-            <Route element={<RouterLayout/>}>
+            <Route
+                path="login"
+                element={<LoginPage/>}
+            />
 
-                <Route
-                    index
-                    element={<ArmyListPage/>}
-                />
 
-                <Route
-                    path="armies/create"
-                    element={<CreateArmyPage/>}
-                />
+            <Route element={<ProtectedRoute/>}>
 
-                <Route
-                    path="armies/:armyId"
-                    element={<ArmyPage/>}
-                />
+                <Route element={<RouterLayout/>}>
+
+                    <Route
+                        index
+                        element={<ArmyListPage/>}
+                    />
+
+                    <Route
+                        path="armies/create"
+                        element={<CreateArmyPage/>}
+                    />
+
+                    <Route
+                        path="armies/:armyId"
+                        element={<ArmyPage/>}
+                    />
+
+                </Route>
 
             </Route>
 
         </Routes>
     )
 }
+
 
 export default App;
